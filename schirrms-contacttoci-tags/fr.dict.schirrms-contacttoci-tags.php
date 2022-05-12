1199f9b83b3e47ce1c1465de5eb70342d5305884
Schirrms <48877593+Schirrms@users.noreply.github.com>
2022-05-12 14:16:34 +0200

    First (non empty) version

--- schirrms-contacttoci-tags/fr.dict.schirrms-contacttoci-tags.php
+++ schirrms-contacttoci-tags/fr.dict.schirrms-contacttoci-tags.php
@@ -6,8 +6,8 @@
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
-Dict::Add('EN US', 'English', 'English', array(
+Dict::Add('FR FR', 'French', 'French', array(
 	// Dictionary entries go here
-	'Class:lnkContactToFunctionalCI/Attribute:relationship' => 'Relationship between contact and CI',
+	'Class:lnkContactToFunctionalCI/Attribute:relationship' => 'Relation entre le contact et le CI',
 ));
 ?>
